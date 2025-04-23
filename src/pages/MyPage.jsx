0d68fd8be9fd2958c153c7334e2b01d0794d1cd1
kjj <jin567656@naver.com>
2025-04-23 17:50:04 +0900

    마이페이지 예약내역

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -12,19 +12,21 @@ const MyPage = () => {
     switch (selectedMenu) {
       case 'reservations':
         return <MyReservations />;
-      // case 'info':
-      //   return <MyInfo />;
-      // case 'reviews':
-      //   return <MyReviews />;
+      // case 'info': return <MyInfo />;
+      // case 'reviews': return <MyReviews />;
       default:
         return <div>콘텐츠를 선택해주세요</div>;
     }
   };
 
   return (
-    <div className="mypage-container">
-      <MySidebar selected={selectedMenu} onSelect={setSelectedMenu} />
-      <div className="mypage-content">{renderContent()}</div>
+    <div className="mypage-wrapper">
+      <div className="mypage-container">
+        <MySidebar selected={selectedMenu} onSelect={setSelectedMenu} />
+        <div className="mypage-content">
+          {renderContent()}
+        </div>
+      </div>
     </div>
   );
 };
