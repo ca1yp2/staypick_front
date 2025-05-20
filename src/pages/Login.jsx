e62c24a692ca89b83b86d9855ee16a168445bc4c
hyeon <seodh2706@gmail.com>
2025-05-20 15:41:57 +0900

    카카오 로그인 작업 중

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../css/Login.css';
 import naverIcon from '../assets/images/naver.png';
 import { RiKakaoTalkFill } from "react-icons/ri";
 import axios from 'axios';
 import { useAuth } from '../context/AuthContext';
+import KakaoLogin from 'react-kakao-login';
 
 const Login = () => {
   const [userid, setUserid] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
   const { setAuth } = useAuth(); // ✅ 컨텍스트 사용
+
+  const kakaoApiKey = import.meta.env.VITE_KAKAO_KEY;
 
   const handleLogin = async () => {
     try {
@@ -43,6 +46,40 @@ const Login = () => {
     }
   };
 
+  //카카오 로그인
+  const kakaoResponse = async (response) => {
+    const { access_token } = response.response;
+
+    try{
+      //백엔드로 카카오 로그인 토큰 전송
+      const res = await axios.post('http://localhost:8081/api/auth/kakao-login', { accessToken: access_token});
+
+      //로그인 성공 후 토큰을 로컬 스토리지에 저장
+      localStorage.setItem('token', res.data);
+
+      //로그인 상태 업데이트
+      const payload = JSON.parse(atob(res.data.split('.')[1]));
+      const user = {
+        username: payload.username,
+        role: payload.role
+      };
+      setAuth({ user, token: res.data });
+      navigate('/');
+    }catch(err){
+      console.error('카카오 로그인 실패', err);
+      alert('카카오 로그인 실패');
+    }
+  };
+
+  useEffect(() => {
+    if (window.Kakao && !window.Kakao.isInitialized()) {
+      window.Kakao.init(kakaoApiKey);
+      console.log("✅ Kakao SDK initialized");
+    } else {
+      console.warn("⚠️ Kakao SDK not loaded yet");
+    }
+  }, [kakaoApiKey]);
+
   return (
     <div className="loginPage">
       <h1>로그인</h1>
@@ -72,7 +109,11 @@ const Login = () => {
       </div>
 
       <div className="social">
-        <button className="kakao-btn"><RiKakaoTalkFill className='kakao-icon' />카카오로 로그인</button>
+        <KakaoLogin
+          token={kakaoApiKey}
+          onSuccess={kakaoResponse}
+          onFailure={kakaoResponse}
+        />
         <button className="naver-btn"><img src={naverIcon} alt="naver-icon" />네이버로 로그인</button>
       </div>
     </div>
